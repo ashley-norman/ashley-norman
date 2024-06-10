c2bfeeeb0cc68c29fbc674a5959f8112a5e43ace
Ashley Norman <ashley.ansell@gmail.com>
2024-06-09 20:17:24 -0400

    feat: about me section

---
+++ app/components/AboutMe.tsx
@@ -0,0 +1,41 @@
+import IAmA from "../IAmA";
+import Image from "next/image";
+import styles from "./about-me.styles.module.css";
+
+export default function AboutMe() {
+  return (
+    <>
+      <div className={`flex flex-col md:flex-row md:gap-10 ${styles.aboutMe}`}>
+        <div className="min-w-fit flex flex-col gap-2">
+          <Image
+            src="/headshot-zoom.jpg"
+            width={250}
+            height={250}
+            alt="Image of Ashley Norman"
+            className={styles.image}
+          />
+          <IAmA className="text-center" />
+        </div>
+        <div className="md:grow leading-loose">
+          <p>
+            I have over 10 years of experience developing web applications. From
+            UX design and UI/API development, to data management and system
+            architecture, I have a track record of doing what needs to be done
+            to bring applications to production and maintain high quality as new
+            features are integrated into deployed applications.
+          </p>
+          <p>
+            I am skilled and/or familiar with tons of different tools & tech. In
+            these times of constantly changing technology, I have foundational
+            knowledge in plain JavaScript, system architecture, and cloud
+            technologies to ensure any application can function at scale. I have
+            meticulous attention to detail and{" "}
+            <span className={`font-bold tracking-widest ${styles.callout}`}>
+              I always sweat the small stuff.
+            </span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,88 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import AboutMe from "./components/AboutMe";
+import React from "react";
+
+type SectionHeaderProps = { title: string; color: string };
+const SectionHeader: React.FC<SectionHeaderProps> = ({ title, color }) => {
+  return (
+    <h2 style={{ color }} className="text-4xl mb-6 font-black tracking-widest">
+      {title}
+    </h2>
+  );
+};
+
+type SectionProps = {
+  title: string;
+  titleColor: string;
+  children: React.ReactNode;
+  className?: string;
+};
+const Section: React.FC<SectionProps> = ({
+  title,
+  titleColor,
+  className,
+  children,
+}) => {
+  return (
+    <section className={`p-7 lg:p-16 ${className ? className : ""}`}>
+      {/* <h2 className="text-4xl mb-6 font-black tracking-widest">About</h2> */}
+      <SectionHeader title={title} color={titleColor} />
+      {children}
+    </section>
+  );
+};
 
 export default function Home() {
   return (
-    <main>
-      <section
-        className={`flex min-h-screen flex-col items-center p-24 ${styles.herobg}`}
+    <>
+      <main>
+        <section
+          className={`flex min-h-screen flex-col items-center p-10 lg:p-24 bg-slate-950 ${styles.texturedBg}`}
+        >
+          <div className="absolute top-0 right-0 text-xs text-white text-opacity-15">
+            Photo by{" "}
+            <a href="https://unsplash.com/@grndezyns?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+              Peter Gargiulo
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/photos/a-black-and-white-photo-of-a-dark-background-cGNCepznaV8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+              Unsplash
+            </a>
+          </div>
+          <div className="grow flex flex-col gap-4 place-items-center text-center justify-center">
+            <h1 className={`text-6xl lg:text-8xl font-bold ${styles.heroText}`}>
+              Ashley Norman
+            </h1>
+            <h2 className="text-white text-xl lg:text-3xl">
+              Full-stack Software Engineer
+            </h2>
+            <button
+              className={`text-white p-4 ${styles.aboutMeButton} ${styles.gradientBorder} hover:bg-white hover:bg-opacity-10 transition`}
+            >
+              About Me
+            </button>
+          </div>
+        </section>
+        <Section title="About" titleColor="#f846ca">
+          <AboutMe />
+        </Section>
+        <Section
+          title="Portfolio"
+          titleColor="#11cf56"
+          className={`bg-slate-950 ${styles.gradientBorder} ${styles.sectionBorder} ${styles.texturedBg}`}
+        >
+          Info here
+        </Section>
+        <Section title="Contact" titleColor="#db9b09">
+          Info here
+        </Section>
+      </main>
+      <footer
+        className={`bg-slate-950 text-white ${styles.footer} ${styles.texturedBg}`}
       >
-        <div className="absolute top-0 right-0 text-xs text-white text-opacity-15">
-          Photo by{" "}
-          <a href="https://unsplash.com/@grndezyns?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-            Peter Gargiulo
-          </a>{" "}
-          on{" "}
-          <a href="https://unsplash.com/photos/a-black-and-white-photo-of-a-dark-background-cGNCepznaV8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-            Unsplash
-          </a>
-        </div>
-        <div className="grow flex flex-col gap-4 place-items-center text-center justify-center">
-          <h1 className={`text-6xl lg:text-8xl font-bold ${styles.animate}`}>
-            Ashley Norman
-          </h1>
-          <h2 className="text-white text-xl lg:text-3xl">
-            Full-stack Software Engineer
-          </h2>
-          {/* <button className="text-white p-4 border-white border rounded-lg hover:bg-white hover:bg-opacity-10 transition">
-            About Me
-          </button> */}
-        </div>
-
-        {/* <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Docs{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Learn{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Templates{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Explore starter templates for Next.js.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Deploy{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div> */}
-      </section>
-      {/* <section className="bg-zinc-100 p-16">
-        <h2 className="text-6xl">About</h2>
-        <div className="flex">
-          <div className="grow">Image here</div>
-          <div className="grow">Details here</div>
-        </div>
-      </section> */}
-    </main>
+        <div className="p-7">Footer</div>
+      </footer>
+    </>
   );
 }
